56039d39df614c4e2b4842453734da399b45c22c
Kelly Dickerson <kdickerson149@gmail.com>
2019-07-26 09:29:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 // Require express 
 // Initialize express app
 // Hook Express up to a localhost
+
+// Next steps: 
+// 1. Figure out how to authenticate Adobe Analytics so our app can access the data
+// 2. Create a route for the What's News page view report
