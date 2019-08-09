563ad2be6cb8ad0e0ec615e3925a67d48b8e0b24
seidel7 <jane.seidel@wsj.com>
2019-08-09 11:40:43 -0400

    added initial app setup

--- server.js
+++ server.js
@@ -6,3 +6,14 @@
 // Next steps: 
 // 1. Figure out how to authenticate Adobe Analytics so our app can access the data
 // 2. Create a route for the What's News page view report
+
+
+const express = require(‘express’);
+const app = express();
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+   console.log(Ready to serve some analytics on port ${server.address().port});
+});
+app.get(‘/health’, function(req, res){
+   res.json(‘Analytics is up and running’);
+});
