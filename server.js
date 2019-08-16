81d42f180f49fc4d83e038dcf52082663669ed45
seidel7 <jane.seidel@wsj.com>
2019-08-16 11:03:37 -0400

    Remove comments

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-// Require express 
-// Initialize express app
-// Hook Express up to a localhost
-// Set up a health route
-
 // Next steps: 
 // 1. Figure out how to authenticate Adobe Analytics so our app can access the data
 // 2. Create a route for the What's News page view report
