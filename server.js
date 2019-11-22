e0ea096b2a351bba737aa8dd4a8bb8b0bec78ae3
Kelly Dickerson <kdickerson149@gmail.com>
2019-11-22 13:34:00 -0500

    Add more set up code, create main api routing

--- server.js
+++ server.js
@@ -5,10 +5,24 @@
 
 const express = require('express');
 const app = express();
+const pageViews = require('./lib/pagviews');
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
    console.log(`Ready to serve some analytics on port ${server.address().port}`);
 });
+
 app.get('/health', function(req, res){
-   res.json('Analytics is up and running');
+   res.json('Analytics is up and running!');
+});
+
+// Hit section delta api in Pubcrawl and get list of articles in add list, and hit packages to round up all articles in TOP_NEWS
+// Loop through each item in top news and hit the adobe api that will return pageviews
+// 
+
+
+app.get('/api/v1/pageviews', (req, res) => {
+    pageViews.getWhatsNewsArticles(function(err, articles) {
+        if (err) res.error({"error": err});
+        res.json(articles);
+    });
 });
